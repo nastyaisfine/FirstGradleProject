a9b68bdaca3d490c7ffe4ee360245806ec1afeae
Анастасия Базанова <nastya.is.fine@mail.ru>
2023-03-10 23:37:14 +0300

    незапускающиеся тесты junit4

--- src/test/CashbackHackServiceTest.java
+++ src/test/CashbackHackServiceTest.java
@@ -2,10 +2,10 @@ package ru.netology.service;
 
 package ru.netology.service;
 
-import static org.testng.AssertJUnit.assertEquals;
+import static org.junit.Assert.assertEquals;
 public class CashbackHackServiceTest {
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void testRemain() {
         CashbackHackService service = new CashbackHackService();
         int amount = 1000;
@@ -14,7 +14,7 @@ public class CashbackHackServiceTest {
         assertEquals(actual,expected);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void testNotEnough() {
         CashbackHackService service = new CashbackHackService();
         int amount = 900;
@@ -24,7 +24,7 @@ public class CashbackHackServiceTest {
 
 
     }
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void testMoreThan1000() {
         CashbackHackService service = new CashbackHackService();
         int amount = 2000;
